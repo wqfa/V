7e6e0dc8432140daea3af83c0108f107118df4ec
XF <118357460+wqfa@users.noreply.github.com>
2023-09-03 22:11:58 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,7 @@
+import telebot, os, time
+bot = telebot.TeleBot('5393921528:AAEMpK64XMx3ALMeNIG82zjD2AhCjzMXowM')
+while True:
+    os.system("wget https://cataas.com/cat  --output-document=cat.jpg")
+    bot.send_photo(chat_id="-1001208654752",photo=open('cat.jpg','rb'))
+    time.sleep(60)
+  
